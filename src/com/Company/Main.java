6390ada6c9206926d09e34cb24824ef64d98339b
TharinduThilakshana0thildezo <gmttdezoysa@students.nsbm.ac.lk>
2025-07-04 14:40:32 +0530

    Updated local variables

---
+++ src/com/Company/Main.java
@@ -0,0 +1,12 @@
+package com.Company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Phone honer = new Phone();
+
+        honer.call();
+        honer.sendText();
+
+    }
+}
