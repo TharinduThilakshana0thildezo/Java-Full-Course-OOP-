a55a5fafc6dd2d89465fb21702e15a4317e30f48
TharinduThilakshana0thildezo <gmttdezoysa@students.nsbm.ac.lk>
2025-07-10 02:31:51 +0530

    Update Wrapper Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,20 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        int num = 13;
+        double d = 12.22;
+        String str = "Hello";
+
+        Integer i = new Integer(num); //boxing -Wrapping
+        Integer h = 14; //auto boxing - warapping
+        Integer l = 21;
+        Integer m =new Integer((int) d);
+
+        System.out.println(i.intValue());//unboxing
+        System.out.println(h.intValue());// and unwrapping
+        System.out.println(l);
+        System.out.println(d);
+
 
     }
 }
