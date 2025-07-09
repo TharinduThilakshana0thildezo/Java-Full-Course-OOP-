70f09d1a4a799de960f54011316483ff86170c7c
TharinduThilakshana0thildezo <gmttdezoysa@students.nsbm.ac.lk>
2025-07-10 02:08:08 +0530

    Create a Cal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,51 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number1;
+        int number2;
+        char operation;
+        int result;
+
+        System.out.print("Enter a number: ");
+        number1 = sc.nextInt();
+
+        System.out.print("What the second number:");
+        number2 = sc.nextInt();
+
+        System.out.print("What is your calculation(+,-,*,/) : ");
+        operation = sc.next().charAt(0);
+
+        if(operation == '+'){
+            result = number1+number2;
+            System.out.println("The Output is: " + result);
+        }
+        else if(operation == '-'){
+            result = number1-number2;
+            System.out.println("The Output is: " + result);
+        }
+        else if(operation == '*'){
+            result = number1*number2;
+            System.out.println("The Output is: " + result);
+
+        }
+        else{
+            result = number1/number2;
+            System.out.println("The Output is: " + result);
+
+        }
+        sc.close();
+
+        System.out.println("Thank you for using our Calculator");
+
+
+
+
 
     }
 }
