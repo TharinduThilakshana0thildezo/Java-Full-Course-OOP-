17f443cbd0402e5eb36085bf03c41e7cc80f19c0
TharinduThilakshana0thildezo <gmttdezoysa@students.nsbm.ac.lk>
2025-07-10 02:13:47 +0530

    Update Raandom

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,15 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
+        Random rand = new Random();
+
+        String name;
+
+        name = rand.nextInt()+"";
+        System.out.println(name);
     }
 }
